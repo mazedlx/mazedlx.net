e62d90dbee038f291159ddfb11f246a62d994b6f
Christian Leo-Pernold <mazedlx@gmail.com>
2018-03-24 07:57:52 +0100

    Secured cookies.

--- config/session.php
+++ config/session.php
@@ -164,7 +164,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -177,7 +177,7 @@ return [
     |
     */
 
-    'http_only' => false,
+    'http_only' => true,
 
     /*
     |--------------------------------------------------------------------------
