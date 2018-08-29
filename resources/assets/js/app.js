877f3c33a766ff27b0d0fd4bf0137f283eaaaa6e
Christian Leo-Pernold <mazedlx@gmail.com>
2018-08-29 10:14:34 +0200

    Remove feather icons js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,11 @@
-
 /**
  * First, we will load all of this project's Javascript utilities and other
  * dependencies. Then, we will be ready to develop a robust and powerful
  * application frontend using useful Laravel and JavaScript libraries.
  */
 
-
-const feather = require("feather-icons");
-feather.replace();
-
 const highlightjs = require("highlight.js");
 highlightjs.initHighlightingOnLoad();
 
-const turbolinks = require("turbolinks")
-turbolinks.start()
+const turbolinks = require("turbolinks");
+turbolinks.start();
