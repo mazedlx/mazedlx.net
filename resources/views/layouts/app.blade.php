3e049f7f337317aed97d850af6385c777d24f840
Christian Leo-Pernold <mazedlx@gmail.com>
2018-08-29 08:22:43 +0200

    Minor cleanup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/main.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans|Source+Code+Pro|Spectral" rel="stylesheet">
     <!-- Scripts -->
-    <script defer src="{{ asset('js/app.js') }}"></script>
+    <script defer src="{{ mix('js/app.js') }}"></script>
 </head>
 <body class="bg-white">
     <div id="app" class="w-full">
