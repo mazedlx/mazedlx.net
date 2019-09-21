f0d5e542fa6f2a549cc9555220df75bfd4386606
Christian Leo-Pernold <mazedlx@gmail.com>
2019-09-21 19:18:39 +0200

    Add back button.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="flex flex-col md:flex-row">
     <div class="mx-auto w-full lg:w-1/2 xl:w-1/2 px-4 py-2">        
+        <div class="mb-4">
+            <a class="border rounded-lg text-lg font-bold px-4 py-2 border-gray-900 hover:bg-gray-900 hover:text-white" href="/">Home</a>
+        </div>
         <div class="font-serif text-5xl font-bold">{{ $post->title}}</div>
         <div class="font-sans text-gray-500 pb-2">{{$post->date->diffForHumans() }}</div>
         <div class="pb-2">{!! $post->contents !!}</div>
