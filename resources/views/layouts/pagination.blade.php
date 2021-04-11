7edb0a8b000ca5fd2afe664eec0cbf2a3bc2adb9
Christian Leo-Pernold <mazedlx@gmail.com>
2021-04-11 16:30:32 +0200

    WIP

--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -1,16 +1,32 @@
 @if ($paginator->hasPages())
-    <ul class="px-4 py-2 list-reset flex flex-row items-center justify-center">
+    <ul class="flex flex-row items-center justify-center px-4 py-2 list-reset">
         @if ($paginator->onFirstPage())
-            <li class="cursor-not-allowed text-gray-500"><span>@lang('pagination.previous')</span></li>
+            <li class="text-gray-500 cursor-not-allowed">
+                <span>@lang('pagination.previous')</span>
+            </li>
         @else
-            <li><a class="text-grey no-underline hover:text-gray-900 hover:underline" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+            <li>
+                <a
+                    class="no-underline text-grey hover:text-gray-900 hover:underline"
+                    href="{{ $paginator->previousPageUrl() }}"
+                    rel="prev"
+                >@lang('pagination.previous')</a>
+            </li>
         @endif
             <li class="px-4"></li>
 
         @if ($paginator->hasMorePages())
-            <li><a class="text-grey no-underline hover:text-gray-900 hover:underline" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+            <li>
+                <a
+                    class="no-underline text-grey hover:text-gray-900 hover:underline"
+                    href="{{ $paginator->nextPageUrl() }}"
+                    rel="next"
+                >@lang('pagination.next')</a>
+            </li>
         @else
-            <li class="cursor-not-allowed text-gray-500"><span>@lang('pagination.next')</span></li>
+            <li class="text-gray-500 cursor-not-allowed">
+                <span>@lang('pagination.next')</span>
+            </li>
         @endif
     </ul>
 @endif
