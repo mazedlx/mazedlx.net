01102296b304727f25308e83a499a2c3fab5338f
Christian Leo-Pernold <christian.leo@akwien.at>
2021-12-12 23:12:44 +0100

    Add newsletter.

---
+++ resources/views/newsletter.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+Hey friends!
+
+I've just published Part 2 of the "From CodeIgniter To Laravel" series on [my blog](https://blog.mazedlx.net/2021/12/12/from-codeigniter-to-laravel-part-2-where-to-start).
+
+Thanks for following along!
+
+-- Christian
+
+[Unsubscribe]({{ route('unsubscribe', $token) }})
+@endcomponent
