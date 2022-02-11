5e8eced0b0dfbf73ef6d9a981c1c61305afd29fe
Christian Leo-Pernold <christian.leo@akwien.at>
2022-02-11 08:13:59 +0100

    WIP.

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -9,13 +9,13 @@
         <p>Are you in need of a website for your company? Or even a web application to digitize your business processes? Bog-standard is not for you? Then you have come to the right place! I craft your website by hand. And your blog. And your CMS. And your super-duper customizable web application. Mobile-first and responsive. From draft to implementation. I'm your one-stop web development shop. I even handle web servers, databases, backups, hosting and other black magic. You have the idea, and I have the fitting technology stack.</p>
 
         <h2>Mobile and Desktop Applications</h2>
-        <p>Would you like a nifty smartphone app? Are you interested in having your web application as a native desktop or mobile app? We would love to advise you!</p>
+        <p>Would you like a nifty smartphone app? Are you interested in having your web application as a native desktop or mobile app? I would love to advise you!</p>
 
         <h2>Trainings</h2>
-        <p>Would you like to bring your developers up to speed? We offer PHP (e.g. testing with PHPUnit and Pest, Laravel) and JavaScript training (e.g. Vue.js, Alpine.js).</p>
+        <p>Would you like to bring your developers up to speed? I offer PHP (e.g. testing with PHPUnit and Pest, Laravel) and JavaScript training (e.g. Vue.js, Alpine.js).</p>
 
         <h2>Consulting</h2>
-        <p>Do you want to focus on your business and not your IT? Then, we are your competent and reliable partner that takes care, beginning with your first computer.</p>
+        <p>Do you want to focus on your business and not your IT? Then, I am your competent and reliable partner that takes care, beginning with your first computer.</p>
     </div>
 </x-page>
 @endsection
