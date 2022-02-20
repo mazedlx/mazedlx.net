be4a585342911c111bab6f3a576bb47944e3f1d6
Christian Leo-Pernold <christian.leo@akwien.at>
2022-02-20 15:40:13 +0100

    Update work.

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -6,7 +6,21 @@
         <h1>Work</h1>
 
         <h2>Web Development</h2>
-        <p>Are you in need of a website for your company? Or even a web application to digitize your business processes? Bog-standard is not for you? Then you have come to the right place! I craft your website by hand. And your blog. And your CMS. And your super-duper customizable web application. Mobile-first and responsive. From draft to implementation. I'm your one-stop web development shop. I even handle web servers, databases, backups, hosting and other black magic. You have the idea, and I have the fitting technology stack.</p>
+        <p>
+            Are you in need of a website for your company? Or even a web application to digitize your business processes? Bog-standard is not for you? Then you have come to the right place! I craft your website by hand. And your blog. And your CMS. And your super-duper customizable web application. Mobile-first and responsive. From draft to implementation. I'm your one-stop web development shop. I even handle web servers, databases, backups, hosting and other black magic. You have the idea, and I have the fitting technology stack. <br><br>
+
+            And speaking of technology stacks, here is mine in case you're interested:
+
+            <ul>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Livewire</li>
+                <li>JavaScript</li>
+                <li>Vue.js / Alpine.js</li>
+                <li>Tailwind CSS</li>
+                <li>MySQL / MariaDB / PostgreSQL</li>
+            </ul>
+        </p>
 
         <h2>Mobile and Desktop Applications</h2>
         <p>Would you like a nifty smartphone app? Are you interested in having your web application as a native desktop or mobile app? I would love to advise you!</p>
