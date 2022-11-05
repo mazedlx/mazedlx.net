d4a152fe5c6ce9313b48cbf69f90e4917a697baa
Christian Leo-Pernold <christian.leo@akwien.at>
2022-11-05 17:25:32 +0100

    Add mastodon verification link.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="IE=edge"
+    >
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
 
     @stack('og')
-
+    <a
+        rel="me"
+        href="https://wien.rocks/@mazedlx"
+    ></a>
     <title>@yield('title')</title>
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta
+        name="csrf-token"
+        content="{{ csrf_token() }}"
+    >
 
     <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link
+        href="{{ mix('css/app.css') }}"
+        rel="stylesheet"
+    >
     @livewireStyles
 </head>
+
 <body class="antialiased bg-gray-50">
-    <div id="app" class="w-full">
+    <div
+        id="app"
+        class="w-full"
+    >
         <x-navigation />
 
         @yield('content')
@@ -25,4 +45,5 @@
     @livewireScripts
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
